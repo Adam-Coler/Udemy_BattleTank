8c587ad87a14a8052c191f214364b0422dac3253
AdamWil_ <adam6457@gmail.com>
2019-01-22 11:16:21 -0700

    Tank Movemnet Compontent

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,6 +4,7 @@
 
 void UTankTrack::SetThrottle(float Throttle) {
 
+
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
